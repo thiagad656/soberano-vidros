d0adfb2ba9224ae6d488ce350843060069cc2ab5
Thiago <thiagodasilva656565@gmail.com>
2025-02-05 21:52:16 -0300

    modificando elementos no views.py

--- app/views.py
+++ app/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')  # Página inicial
 
-def sobre(request):
-    return render(request, 'sobre.html')  # Página sobre
+def aboutus(request):
+    return render(request, 'aboutus.html')  # Página sobre
 
-def galeria(request):
-    return render(request, 'galeria.html')  # Página galeria
+def galery(request):
+    return render(request, 'galery.html')  # Página galeria
 
-def contato(request):
-    return render(request, 'contato.html')  # Página contato
+def contacts(request):
+    return render(request, 'contacts.html')  # Página contato
+
+def footer(request):
+    return render(request, 'footer.html')  # Página rodapé
